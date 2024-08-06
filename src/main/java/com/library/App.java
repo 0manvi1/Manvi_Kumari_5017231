ee7e89cf627c9490d006c51dd8e519a2f49a08cb
0manvi1 <manvi0392@gmail.com>
2024-08-06 16:56:22 +0530

    add

---
+++ src/main/java/com/library/App.java
@@ -0,0 +1,22 @@
+package com.library;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+{      
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Get BookService bean and test
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Add your test logic here
+        bookService.message();
+    }
+}
---
+++ src/main/java/com/library/service/BookService.java
@@ -0,0 +1,7 @@
+package com.library.service;
+
+public class BookService {
+    public void message(){
+		System.out.println("Book taken");
+	}
+}
